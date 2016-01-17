7bf6d5fd9e125abaa503b31e7425859466a62e3d
Serg Tatarnikov (sergtat) <sergeytat@gmail.com>
2016-01-17 20:26:46 +0400

    modify setup

--- setup.sh
+++ setup.sh
@@ -5,24 +5,24 @@ src=$dst/.dotfiles
 
 source	$src/colormap
 
-name='Oh-my-zsh'
-cecho "Installing $name" $Yellow
-rm -rf ~/.oh-my-zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-[[ $? ]] && cecho "... $name done" $Green || cecho "... $name Error" $Red
+# name='Oh-my-zsh'
+# cecho "Installing $name" $Yellow
+# rm -rf ~/.oh-my-zsh
+# git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+# [[ $? ]] && cecho "... $name done" $Green || cecho "... $name Error" $Red
 
-name='Powerline fonts'
-cecho "Installing $name" $Yellow
-TD=$(mktemp -d)
-git clone https://github.com/powerline/fonts.git $TD
-$TD/install.sh
-[[ $? ]] && cecho "... $name done" $Green || cecho "... $name Error" $Red
-rm -rf $TD
-unset TD
+# name='Powerline fonts'
+# cecho "Installing $name" $Yellow
+# TD=$(mktemp -d)
+# git clone https://github.com/powerline/fonts.git $TD
+# $TD/install.sh
+# [[ $? ]] && cecho "... $name done" $Green || cecho "... $name Error" $Red
+# rm -rf $TD
+# unset TD
 
 readfile() {
 local o=$IFS
-IFS=$(echo -en "\n")
+IFS=$(echo -en "\n\b")
 FN="$(cat $@)"
 for i in $FN
 do 
