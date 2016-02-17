fca7361cc99c1c9a1ad79c7e7f85e69b56d42c2b
Serg Tatarnikov (sergtat) <sergeytat@gmail.com>
2016-02-17 08:24:32 +0400

    Update

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,22 +5,24 @@ src=$HOME/repo/dotfiles
 
 source	$src/bin/colormap
 
-#name='Oh-my-zsh'
-#cecho "Installing $name" $Yellow
-#rm -rf $dst/.oh-my-zsh
-#git clone https://github.com/robbyrussell/oh-my-zsh.git $dst/.oh-my-zsh
-#[[ $? ]] && cecho "... $name done" $Green || cecho "... $name Error" $Red
+name='Oh-my-zsh'
+cecho "Installing $name" $Yellow
+rm -rf $dst/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git $dst/.oh-my-zsh
+[[ $? ]] && cecho "... $name done" $Green || cecho "... $name Error" $Red
 unset name
 
-#name='Powerline fonts'
-#cecho "Installing $name" $Yellow
-#TD=$(mktemp -d)
-#git clone https://github.com/powerline/fonts.git $TD
-#$TD/install.sh
-#[[ $? ]] && cecho "... $name done" $Green || cecho "... $name Error" $Red
-## rm -rf $TD
-#unset TD
+name='Powerline fonts'
+cecho "Installing $name" $Yellow
+TD=$(mktemp -d)
+git clone https://github.com/powerline/fonts.git $TD
+$TD/install.sh
+[[ $? ]] && cecho "... $name done" $Green || cecho "... $name Error" $Red
+rm -rf $TD
+unset TD
 unset name
+
+exit 0
 
 readfile() {
 local o=$IFS
